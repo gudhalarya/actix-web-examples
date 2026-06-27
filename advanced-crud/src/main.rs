418221af27eb85f2cebe0bc6ee8610413112a8a7
Draken1974 <gudhalarya@gmail.com>
2026-06-27 10:54:36 +0530

    Advanced added

--- advanced-crud/src/main.rs
+++ advanced-crud/src/main.rs
@@ -1,47 +1,56 @@
-use std::env;
-
-use actix_web::{App, HttpResponse, HttpServer, web};
+use actix_web::{App, HttpServer, web};
+use argon2::{
+    Argon2, PasswordHasher,
+    password_hash::{SaltString, rand_core::OsRng},
+};
 use sqlx::{PgPool, postgres::PgPoolOptions};
-//We will suse custom error handling here
+use std::env;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum AppError {
-    #[error("Hashing Error")]
-    HashingError(#[from] argon2::Error),
-
-    #[error("Database Error")]
+    #[error("Hashing error")]
+    HashingError(#[from] argon2::password_hash::Error),
+    #[error("Database error")]
     DatabaseError(#[from] sqlx::Error),
 }
 
 impl actix_web::ResponseError for AppError {
+    // fixed trait name
     fn error_response(&self) -> actix_web::HttpResponse<actix_web::body::BoxBody> {
         match self {
             AppError::HashingError(_) => {
-                HttpResponse::InternalServerError().json("Hashing error occured")
+                actix_web::HttpResponse::InternalServerError().json("Hashing error occurred")
             }
             AppError::DatabaseError(_) => {
-                HttpResponse::InternalServerError().json("Database error occured")
+                actix_web::HttpResponse::InternalServerError().json("Database error occurred")
             }
         }
     }
 }
 
 async fn get_db() -> Result<PgPool, AppError> {
-    let db_url =
-        env::var("DATABASE_URL").expect("Could not find the database url in the env file ");
+    let db_url = env::var("DATABASE_URL").expect("Could not find the database url in the env file");
     let pool = PgPoolOptions::new()
         .max_connections(10)
-        .min_connections(5)
-        .acquire_timeout(std::time::Duration::from_secs(3))
+        .min_connections(2)
+        .acquire_timeout(std::time::Duration::from_secs(5))
         .connect(&db_url)
         .await?;
     Ok(pool)
 }
 
+fn hash_password(password: &str) -> Result<String, AppError> {
+    let salt = SaltString::generate(&mut OsRng);
+    let hashed_password = Argon2::default()
+        .hash_password(password.as_bytes(), &salt)?
+        .to_string(); // convert to String
+    Ok(hashed_password)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let pool = get_db().await.expect("Could not get the database");
+    let pool = get_db().await.expect("Could not connect to the db");
     HttpServer::new(move || App::new().app_data(web::Data::new(pool.clone())))
         .bind(("127.0.0.1", 8080))?
         .run()
